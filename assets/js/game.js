b10a1e3e9eaf9cfd1a93c883de41a4a748d60813
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-09-19 21:11:34 -0600

    working on fight function to have players attack one another and check for remaining life points

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,42 @@
 
 var playerName = window.prompt("What is you robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The Fight Has Begun!")
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+//could also be written "function fight() {  }"
+var fight = function() {
+    window.alert("Welcome To Robot Gladiators!")
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-//fight();
+fight();
