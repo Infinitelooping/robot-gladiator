f0af4021eaaacf332a21fce2f879075150796a73
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-09-21 19:41:40 -0600

    enhanced battles, with multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,52 +10,55 @@ var enemyAttack = 12;
 
 //could also be written "function fight() {  }"
 var fight = function(enemyName) {
-    window.alert("Welcome To Robot Gladiators!")
 
+
+    while(enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to fight or skip this battle? Enter FIGHT or SKIP.");
     console.log(promptFight);
 
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-        console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
-        playerAttacks();
-        if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
-        } 
-        else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        if (promptFight === "fight" || promptFight === "FIGHT") {
+        
+            console.log(
+                playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            );
+            playerAttacks();
+            if (enemyHealth <= 0) {
+                window.alert(enemyName + " has died!");
+            } 
+            else {
+                window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            };
+
+            enemyAttacks();
+            console.log(
+                enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            );
+
+            if (playerHealth <= 0) {
+                window.alert(playerName + " has died!");
+            } 
+            else {
+                window.alert(playerName + " still has " + playerHealth + " health left.");
+            };
+        } else if (promptFight === "skip" || promptFight === "SKIP") {
+            // confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+            // if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // subtract money from playerMoney for skipping
+                playerMoney = playerMoney - 2;
+            }
+            // if no (false), ask question again by running fight() again
+            else {
+                fight();
+            } 
+        } else {
+            window.alert("You need to choose a valid option. Try again!");
         };
-
-        enemyAttacks();
-        console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-        );
-
-        if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-        } 
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        };
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
-        }
-        // if no (false), ask question again by running fight() again
-        else {
-            fight();
-        } 
-    } else {
-        window.alert("You need to choose a valid option. Try again!");
     };
-}
+};
 
 //helper function for when the player attacks Robot
 var playerAttacks = function() {
@@ -67,7 +70,9 @@ var enemyAttacks = function() {
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   };
 //fight();
 
